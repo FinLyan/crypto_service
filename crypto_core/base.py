feb169b1350c02f09b35902335f488c1129b6947
FinLyan <finndan2006@gmail.com>
2026-08-20 17:53:58 +0300

    fix base cipher import path and built the skeleton of the cipher class

--- crypto_core/base.py
+++ crypto_core/base.py
@@ -1,10 +1,30 @@
-from exceptions import InvalidKeyError, InvalidTextError
+from .exceptions import InvalidKeyError, InvalidTextError
+
 class BaseCipher:
+    cipher_id = ""
+    cipher_name = ""
+    description = ""
+    key_hint = ""
+    def _text_validator(self, text):
+        if not isinstance(text, str) or text == "":
+            raise InvalidTextError ("Текст должен быть не пустой строкой")
 
-
-
-    def _keyValidate(self, key):
+    def _key_validator(self, key):
         if not isinstance(key, str) or key == "":
-            raise InvalidKeyError
-    def _textValidate(self, text):
-        if not isinstance
+            raise InvalidKeyError ("Ключ должен быть не пустой строкой")
+        
+    def encrypt(self, text, key):
+        self._key_validator(key)
+        self._text_validator(text)
+        return self._encrypt(text, key)
+
+    def decrypt(self, text, key):
+        self._key_validator(key)
+        self._text_validator(text)
+        return self._decrypt(text, key)
+
+    def _encrypt(self, text, key):
+        raise NotImplementedError
+    
+    def _decrypt(self, text, key):
+        raise NotImplementedError
