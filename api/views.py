a11ef40cd4a1936ad83c5fbfdc298c0b2f20ac85
FinLyan <finndan2006@gmail.com>
2026-08-22 15:47:52 +0300

    add GET /api/v1/ciphers endpoint

--- api/views.py
+++ api/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from crypto_core.registry import list_ciphers
 
-# Create your views here.
+@api_view(["GET"])
+def ciphers_list(request):
+    return Response(list_ciphers())
